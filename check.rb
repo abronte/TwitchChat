a5d1afd3bc0287544742d8664e9ccb9512049e7d
Adam Bronte <abronte@gmail.com>
2014-04-29 20:15:56 -0700

    make the lists side by side and increase them to top 20

--- check.rb
+++ check.rb
@@ -6,8 +6,8 @@ r = Redis.new(port: 8888)
 word_count = r.zcount "words", 1, 99999999
 msg_count = r.zcount "chat", 1, 99999999
 
-words = r.zrevrange "words", 0, 15, :with_scores => true
-msgs = r.zrevrange "chat", 0, 15, :with_scores => true
+words = r.zrevrange "words", 0, 19, :with_scores => true
+msgs = r.zrevrange "chat", 0, 19, :with_scores => true
 
 if ARGV[0]
   data = {
