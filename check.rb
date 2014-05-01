aab437000a4f6c34e94ae464b81b046f5e45e35f
Adam Bronte <abronte@gmail.com>
2014-04-30 20:27:03 -0700

    total should be an int

--- check.rb
+++ check.rb
@@ -17,7 +17,7 @@ if ARGV[0]
     messages: msgs,
     total_messages: msg_count,
     total_words: word_count,
-    total_processed: total
+    total_processed: total.to_i
   }
 
   File.write(ARGV[0], data.to_json)
